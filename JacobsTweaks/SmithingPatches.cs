63e45710a2358589177744766652deb6fd3623fb
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2020-04-04 14:44:09 -0400

    creates smithing patches and adds modifier to smithing level needed for new parts

---
+++ JacobsTweaks/SmithingPatches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+
+namespace JacobsTweaks
+{
+    [HarmonyPatch(typeof(DefaultSmithingModel), "ResearchPointsNeedForNewPart")]
+    class SmithingPatches
+    {
+        static void Postfix(ref int __result)
+        {
+            __result /= 18;
+        }
+    }
+}
